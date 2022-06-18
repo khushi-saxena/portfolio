454227219ec132ffb65a5aae271baa1961c26039
Khushi Saxena <khushisaxena123123@gmail.com>
2022-06-18 15:31:56 +0530

    Add changes in about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,8 +17,9 @@ const About = () => {
                 <b>Research Intern</b>
                 <p>Working as Research Intern at INMAS, Defence Research and Development Organization (DRDO)</p>
                 <b>DG Techno Soft</b>
+                <p >
                 I worked as an Full-Stack Dev Intern at DG Techno Soft India.Here I am Developed a website from scratch using React Framework,REST-APIs and developed ongoing projects and Made their UI better using different languages and framework
-                 {/* <a className="about-link-element" href="https://drive.google.com/file/d/1aCpj38kxO8S2cMg-6hZc2pqv1MndPk0u/view">.Check here!</a> */}
+                </p> {/* <a className="about-link-element" href="https://drive.google.com/file/d/1aCpj38kxO8S2cMg-6hZc2pqv1MndPk0u/view">.Check here!</a> */}
               </p>
               <h3 className="about-sub-head">Open Source Contributor</h3>
               <p className="about details">
